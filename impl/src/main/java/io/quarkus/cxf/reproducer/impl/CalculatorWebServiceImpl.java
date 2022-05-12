18ff04354e8bda8b8948bcb70f20e435a8ad0be3
Robert Delbrueck <robert.delbrueck@bdr.de>
2022-05-12 15:15:02 +0200

    remove RequestScoped

--- impl/src/main/java/io/quarkus/cxf/reproducer/impl/CalculatorWebServiceImpl.java
+++ impl/src/main/java/io/quarkus/cxf/reproducer/impl/CalculatorWebServiceImpl.java
@@ -1,11 +1,9 @@
 package io.quarkus.cxf.reproducer.impl;
 
 import io.quarkus.cxf.reproducer.api.ws.CalculatorWebService;
-import javax.enterprise.context.RequestScoped;
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
 
-@RequestScoped
 @WebService(targetNamespace = "http://tempuri.org/", serviceName = "Calculator")
 @BindingType(javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
 public class CalculatorWebServiceImpl implements CalculatorWebService {
